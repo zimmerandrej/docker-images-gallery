5820969ca533f2594452ce854344da60bd02e5d3
Andrej <andrej.zimmer@outlook.com>
2024-01-21 14:05:30 +0100

    main

--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -18,7 +18,7 @@ const Search = ({ word, setWord, handleSubmit }) => {
               </Col>
               <Col>
                 <Button variant="primary" type="submit">
-                  Search Images
+                  Search Image
                 </Button>
               </Col>
             </Form.Row>
